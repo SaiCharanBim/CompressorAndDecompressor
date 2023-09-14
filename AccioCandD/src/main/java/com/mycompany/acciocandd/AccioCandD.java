aeb4f4283124eb3de6b19675a3c46d1ac7272e5d
saicharanbim <107110377+SaiCharanBim@users.noreply.github.com>
2023-09-14 17:55:30 +0530

    Add files via upload

---
+++ AccioCandD/src/main/java/com/mycompany/acciocandd/AccioCandD.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.acciocandd;
+
+/**
+ *
+ * @author BIM SAI CHARAN
+ */
+public class AccioCandD {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
